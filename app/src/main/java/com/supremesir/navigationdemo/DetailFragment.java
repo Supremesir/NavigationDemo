f30cf16f85cb5599805c5cf4f75c3fce7a5c429f
Haofan Fang <haofanfang@gmail.com>
2020-03-12 17:57:27 +0800

    update animation

--- app/src/main/java/com/supremesir/navigationdemo/DetailFragment.java
+++ app/src/main/java/com/supremesir/navigationdemo/DetailFragment.java
@@ -2,7 +2,9 @@ package com.supremesir.navigationdemo;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,5 +26,12 @@ public class DetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getActivity().findViewById(R.id.button2).
+                setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_detailFragment2_to_homeFragment2));
     }
 }
